e0de8a69536b4ebab1b12c6a9539d598fd33e5fa
Jack Christensen <jack@jackchristensen.com>
2022-09-03 19:38:01 -0500

    Initial commit

---
+++ structure.sql
@@ -0,0 +1,24 @@
+create extension if not exists pgcrypto;
+
+create table extid_types (
+	prefix text not null primary key,
+	secret_key bytea not null default gen_random_bytes(16)
+);
+
+create or replace function encode_extid(prefix text, id bigint)
+returns text
+stable
+language sql as $$
+	select extid_types.prefix || '_' || encode(encrypt(int8send(id), secret_key, 'aes-ecb/pad:none'), 'hex')
+	from extid_types
+	where extid_types.prefix = $1;
+$$;
+
+create or replace function decode_extid(extid text)
+returns bigint
+stable
+language sql as $$
+	select ('x' || left(encode(decrypt(decode(split_part($1, '_', 2), 'hex'), secret_key, 'aes-ecb/pad:none'), 'hex'), 16))::bit(64)::bigint
+	from extid_types
+	where extid_types.prefix = split_part($1, '_', 1);
+$$;
